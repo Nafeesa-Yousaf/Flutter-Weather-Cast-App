dd584dc035474faedb1b01375440eefce4e8afe2
Nafeesa Yousaf <nafeesayousaf2129@gmail.com>
2023-08-25 20:04:34 +0500

    Weather App

--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:weather/Weater_Home.dart';
 
 class Splash_Screen extends StatefulWidget {
@@ -11,7 +12,6 @@ class Splash_Screen extends StatefulWidget {
 }
 
 class _Splash_ScreenState extends State<Splash_Screen> {
-
   @override
   void initState() {
     // TODO: implement initState
@@ -23,14 +23,35 @@ class _Splash_ScreenState extends State<Splash_Screen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: const Color.fromARGB(255, 0, 33, 59),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [Color.fromARGB(255, 11, 30, 46), // Blue color
+      Color.fromARGB(255, 68, 27, 24),
+      Color(0xff081b25),
+        ],
+        begin: Alignment.bottomCenter,
+        end: Alignment.topCenter,
+        stops: [0.3, 0.5, 1.0],
+      )),
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image(
-                image: AssetImage("WeatherSplash.png"), width: MediaQuery.of(context).size.width*0.3, height: MediaQuery.of(context).size.height*0.3),
-                
+                image: AssetImage("splash.png"),
+                width: MediaQuery.of(context).size.width * 0.3,
+                height: MediaQuery.of(context).size.height * 0.3),
+                SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20,right: 10),
+                    child: Text(
+                      "Discover The Weather In your City",
+                      style: GoogleFonts.poppins(
+                          color: Colors.white, fontSize: 40,decoration: TextDecoration.none),textAlign: TextAlign.center,
+                    ),
+                  ),
           ],
         ),
       ),
